257aef36fe4dfb9b358d740226f098ca3c1b4903
jerielng <jeriel.ng@gmail.com>
2018-09-08 23:52:09 -0500

    Added menu links to external sites

--- app/src/main/java/com/auburnhacks/auburnhacksmobile/MainActivity.java
+++ app/src/main/java/com/auburnhacks/auburnhacksmobile/MainActivity.java
@@ -1,5 +1,7 @@
 package com.auburnhacks.auburnhacksmobile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,9 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.auburnhacks.auburnhacksmobile.views.FaqFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FaqFragment mFaqFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_schedule:
                     return true;
                 case R.id.navigation_faq:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container, mFaqFragment)
+                            .commit();
                     return true;
                 case R.id.navigation_sponsors:
                     return true;
@@ -32,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFaqFragment = new FaqFragment();
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -48,8 +59,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.settings_register:
+                String registerUrl = getString(R.string.url_registration);
+                Intent registerIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(registerUrl));
+                startActivity(registerIntent);
                 return true;
             case R.id.settings_slack:
+                String slackUrl = getString(R.string.url_slack);
+                Intent slackIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(slackUrl));
+                startActivity(slackIntent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/auburnhacks/auburnhacksmobile/views/FaqFragment.java
@@ -0,0 +1,25 @@
+package com.auburnhacks.auburnhacksmobile.views;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.auburnhacks.auburnhacksmobile.R;
+
+public class FaqFragment extends Fragment {
+
+    public FaqFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_faq, container, false);
+    }
+}
