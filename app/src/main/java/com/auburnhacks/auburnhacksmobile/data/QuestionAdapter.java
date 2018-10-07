f6c342f2d1d07eecf8015371cfaaccf85de6c3af
Bradley Probert <bradprobert96@gmail.com>
2018-10-07 14:53:54 -0500

    Finished FAQ tab

--- app/src/main/java/com/auburnhacks/auburnhacksmobile/data/QuestionAdapter.java
+++ app/src/main/java/com/auburnhacks/auburnhacksmobile/data/QuestionAdapter.java
@@ -2,9 +2,15 @@ package com.auburnhacks.auburnhacksmobile.data;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.auburnhacks.auburnhacksmobile.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,38 +22,48 @@ public class QuestionAdapter
         extends RecyclerView.Adapter<QuestionAdapter.QuestionAdapterViewHolder> {
 
     public Context mContext;
-    private ArrayList<String> questionList;
-    private ArrayList<String> answerList;
+    private ArrayList<Question> questionList;
 
     public static final String PARAM_INFO_CARDS = "info_cards";
     public static final String PARAM_QUESTION = "question";
     public static final String PARAM_ANSWER = "answer";
 
     public class QuestionAdapterViewHolder extends RecyclerView.ViewHolder {
-        public QuestionAdapterViewHolder(LinearLayout view) {
-            super(view);
+        public TextView questionTv;
+        public TextView answerTv;
+        public QuestionAdapterViewHolder(View v) {
+            super(v);
+            questionTv = (TextView) v.findViewById(R.id.question);
+            answerTv = (TextView) v.findViewById(R.id.answer);
         }
     }
 
     public QuestionAdapter(Context context, String jsonString) {
         mContext = context;
+        questionList = new ArrayList<>();
         extractData(jsonString);
     }
 
     @NonNull
     @Override
     public QuestionAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.question_view, parent, false);
+        QuestionAdapterViewHolder vh = new QuestionAdapterViewHolder(view);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull QuestionAdapterViewHolder holder, int position) {
-
+        String question = questionList.get(position).getQuestion();
+        String answer = questionList.get(position).getAnswer();
+        holder.questionTv.setText(question);
+        holder.answerTv.setText(answer);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return questionList.size();
     }
 
     public void extractData(String jsonString) {
@@ -57,6 +73,7 @@ public class QuestionAdapter
             for (int i = 0; i < questionResults.length(); i++) {
                 String question = questionResults.getJSONObject(i).optString(PARAM_QUESTION);
                 String answer = questionResults.getJSONObject(i).optString(PARAM_ANSWER);
+                questionList.add(new Question(question, answer));
             }
         } catch (JSONException e) {
             e.printStackTrace();
