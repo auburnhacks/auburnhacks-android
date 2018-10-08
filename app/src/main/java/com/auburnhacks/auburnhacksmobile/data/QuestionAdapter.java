12a1056ce0329c97640130c36fe26571490e7ebe
Bradley Probert <bradprobert96@gmail.com>
2018-10-07 19:22:23 -0500

    Finished FAQ tab

--- app/src/main/java/com/auburnhacks/auburnhacksmobile/data/QuestionAdapter.java
+++ app/src/main/java/com/auburnhacks/auburnhacksmobile/data/QuestionAdapter.java
@@ -2,12 +2,10 @@ package com.auburnhacks.auburnhacksmobile.data;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.auburnhacks.auburnhacksmobile.R;
